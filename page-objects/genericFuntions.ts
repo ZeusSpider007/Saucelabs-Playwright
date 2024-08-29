112f28180331c942e0205739dcb18ac2008dac61
Nayan Wankhade <104750879+ZeusSpider007@users.noreply.github.com>
2024-08-29 21:59:37 +0530

    added login-page verification

---
+++ page-objects/genericFuntions.ts
@@ -0,0 +1,14 @@
+import { Locator, Page, expect } from "@playwright/test";
+
+
+// Function to highlight an element using Locator
+export async function highlightElement(element: Locator) {
+    await element.evaluate((el) => {
+      if (el instanceof HTMLElement) {
+        el.style.outline = '1px solid red'; // Apply outline style
+        setTimeout(() => (el.style.outline = ''), 1000); // Remove the outline after 1 second
+      }
+    });
+  }
+
+
---
+++ page-objects/loginPage.ts
@@ -0,0 +1,61 @@
+import { Page, expect } from "@playwright/test";
+import * as utils from "./genericFuntions"
+
+export class LoginPage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async verifyLoginPageHeaderText() {
+    const headerLogo = this.page.getByText("Swag Labs");
+    await utils.highlightElement(headerLogo)
+    await expect(headerLogo).toBeVisible();
+  }
+
+  async verifyLoginPageUsernameField() {
+
+    const UsernameTextField = this.page.locator('[data-test="username"]')
+    await utils.highlightElement(UsernameTextField)
+    await expect(UsernameTextField).toBeVisible();
+    await expect(UsernameTextField).toBeEditable();
+  }
+
+  async verifyLoginPagePasswordField() {
+
+    const passwordTextField = this.page.locator('[data-test="password"]');
+    await utils.highlightElement(passwordTextField)
+    await expect(passwordTextField).toBeVisible();
+    await expect(passwordTextField).toBeEditable();
+  }
+
+
+  async verifyLoginButton() {
+    const loginButton = this.page.locator('[data-test="login-button"]');
+    await utils.highlightElement(loginButton)
+    await expect(loginButton).toBeEnabled();
+    await expect(loginButton).toHaveCSS('background-color', "rgb(61, 220, 145)")
+
+  }
+
+  async verifyLoginCredpanel() {
+    const loginCredentialsUsernames = this.page.locator('[data-test="login-credentials"]');
+    await utils.highlightElement(loginCredentialsUsernames)
+    await expect(loginCredentialsUsernames).toBeVisible();
+  }
+
+  async verifyPassCredpanel() {
+    const loginCredentialsPass = this.page.locator('[data-test="login-password"]');
+    await utils.highlightElement(loginCredentialsPass)
+    await expect(loginCredentialsPass).toBeVisible();
+  }
+
+  async verifyCredentialDomPanel() {
+    const credentialsdom = this.page.locator('[data-test="login-credentials-container"] div').first();
+    await utils.highlightElement(credentialsdom)
+    expect(credentialsdom).toHaveCSS("background-color", "rgb(19, 35, 34)");
+
+  }
+
+}
